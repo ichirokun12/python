3374897f43916fe792cad1b9377d0824594e56fb
Rajat Singh <135345654+ichirokun12@users.noreply.github.com>
2024-08-05 18:46:05 +0530

    Create file_operation.py

---
+++ file_operation.py
@@ -0,0 +1,25 @@
+string1 = "a string "
+length = len(string1)
+print("length of string is = ", length)
+
+Upper = string1.upper()
+print("Upper case : ", Upper)
+
+
+myfile = open("file.txt", "r")
+lines = myfile.radline()
+for line in lines
+print(line)
+
+src = input("enter source file")
+destn = input("enter destination ")
+
+f1 = open("src", "r")
+data = f1.read()
+f1.close()
+
+f2 = open("destn", "w")
+data = f2.write();
+f2.close()
+
+print("file copied ")
